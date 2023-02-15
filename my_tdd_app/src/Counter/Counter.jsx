f757e27daa44873cb0c2ac1363fea42df43d54f8
deinde dawodu <ddawodu@brightsign.biz>
2023-02-15 13:25:14 -0500

    adding in additional test

--- my_tdd_app/src/Counter/Counter.jsx
+++ my_tdd_app/src/Counter/Counter.jsx
@@ -1,14 +1,41 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import counterStyle from "./Counter.module.css";
 
 export default function Counter() {
-    const [counter,setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState(1);
+  console.log(counterStyle["green"])
+
+  const addToCounter = () => {
+    setCounter(counter + input);
+  };
+
+  const subFromCounter = () => {
+    setCounter(counter - input);
+  };
   return (
     <div>
       <h1 data-testid="header">My Counter</h1>
-      <h2 data-testid="counter">{counter}</h2>
-      <button data-testid="subtractBtn">-</button>
-      <input data-testid="input" type="text" value="1" />
-      <button data-testid="addBtn">+</button>
+      <h2 data-testid="counter"
+      className={`${counter >= 100? counterStyle.green:""}${counter <=-100 ? counterStyle.red:""}` }
+      >
+      {counter}
+      </h2>
+      <button onClick={subFromCounter} data-testid="subtractBtn">
+        -
+      </button>
+      <input
+        className={counterStyle.center}
+        data-testid="input"
+        type="number"
+        value={input}
+        onChange={(e) => {
+          setInput(parseInt(e.target.value));
+        }}
+      />
+      <button onClick={addToCounter} data-testid="addBtn">
+        +
+      </button>
     </div>
   );
 }
--- my_tdd_app/src/Counter/__test__/Counter.test.js
+++ my_tdd_app/src/Counter/__test__/Counter.test.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Counter from "../Counter";
-import { render, screen } from "@testing-library/react";
+import { render, screen,fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
+
+
+//RENDERING PROPERLY TEST
 test("header renders with correct text", () => {
   const {getByTestId} = render(<Counter />);
 
@@ -35,8 +38,165 @@ test("add button render with + sign",()=>{
 
 test("add button render with - sign",()=>{
     const {getByTestId} = render(<Counter/>)
-    const addBtnEl = getByTestId("subtractBtn")
+    const subBtnEl = getByTestId("subtractBtn")
 
-    expect(addBtnEl.textContent).toBe("-")
+    expect(subBtnEl.textContent).toBe("-")
 })
 
+
+//FUNTIONALITY TEST
+
+test("change value of input works correctly",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const inputEl = getByTestId("input")
+
+    expect(inputEl.value).toBe("1")
+
+    fireEvent.change(inputEl,{
+        target:{
+            value:5
+        }
+    })
+
+    expect(inputEl.value).toBe("5")
+})
+
+test("when + button clicked value should add 1 to counter value",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const addBtnEl = getByTestId("addBtn")
+    const counterEl = getByTestId("counter")
+    
+    expect(counterEl.textContent).toBe("0")
+    
+    fireEvent.click(addBtnEl);
+
+
+    expect(counterEl.textContent).toBe("1")
+})
+
+
+
+test("when - button clicked value should subtract 1 from counter value",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const subBtnEl = getByTestId("subtractBtn")
+    const counterEl = getByTestId("counter")
+    
+    expect(counterEl.textContent).toBe("0")
+    
+    fireEvent.click(subBtnEl);
+
+
+    expect(counterEl.textContent).toBe("-1")
+})
+
+
+test("changing input value then clicking on add button works in sync values",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const addBtnEl = getByTestId("addBtn")
+    const counterEl = getByTestId("counter")
+    const inputEl = getByTestId("input")
+    
+    fireEvent.change(inputEl,{
+        target:{
+            value:5
+        }
+    })
+    fireEvent.click(addBtnEl);
+
+
+    expect(counterEl.textContent).toBe("5")
+})
+test("changing input value then clicking on add button works in sync values",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const addBtnEl = getByTestId("subtractBtn")
+    const counterEl = getByTestId("counter")
+    const inputEl = getByTestId("input")
+    
+    fireEvent.change(inputEl,{
+        target:{
+            value:5
+        }
+    })
+    fireEvent.click(addBtnEl);
+
+
+    expect(counterEl.textContent).toBe("-5")
+})
+
+test("adding then subtracting leads to the correct counter number",()=>{
+   const {getByTestId} =render(<Counter/>)
+   const addBtnEl = getByTestId("addBtn")
+   const subBtnEl = getByTestId("subtractBtn")
+   const counter = getByTestId("counter")
+   const inputEl = getByTestId("input")
+
+   expect(counter.textContent).toBe("0")
+
+
+   fireEvent.change(inputEl,{
+    target:{
+        value:10
+    }
+   })
+   fireEvent.click(addBtnEl)
+   fireEvent.click(addBtnEl)
+   fireEvent.click(addBtnEl)
+   fireEvent.click(addBtnEl)
+
+   fireEvent.click(subBtnEl)
+   fireEvent.click(subBtnEl)
+
+   expect(counter.textContent).toBe("20")
+
+   fireEvent.change(inputEl,{
+    target:{
+        value:5
+    }
+   })
+
+   fireEvent.click(addBtnEl)
+
+   fireEvent.click(subBtnEl)
+   fireEvent.click(subBtnEl)
+   
+   expect(counter.textContent).toBe("15")
+
+})
+
+
+test("counter contains correct class name",()=>{
+    const {getByTestId} = render(<Counter/>)
+    const counter = getByTestId("counter")
+    const inputEl = getByTestId("input")
+    const addBtnEl = getByTestId("addBtn")
+    const subBtnEl = getByTestId("subtractBtn")
+
+    fireEvent.click(inputEl,{
+        target:{
+            value:50
+        }
+    })
+
+    fireEvent.click(addBtnEl)
+    
+    expect(counter.className).toBe("")
+
+    fireEvent.click(addBtnEl)
+
+    expect(counter.className).toBe("green")
+
+    fireEvent.click(addBtnEl)
+
+    expect(counter.className).toBe("green")
+    fireEvent.click(subBtnEl)
+    fireEvent.click(subBtnEl)
+    expect(counter.className).toBe("")
+
+    fireEvent.click(subBtnEl)
+    fireEvent.click(subBtnEl)
+    fireEvent.click(subBtnEl)
+    fireEvent.click(subBtnEl)
+
+    expect(counter.className).toBe("red")
+
+})
